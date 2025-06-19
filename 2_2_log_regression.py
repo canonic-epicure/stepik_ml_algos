aa22398494ca204fd60b1fe5063216c4de9af5b9
nickolay <nickolay8@gmail.com>
2025-06-19 19:40:18 +0300

    progress on 2_2

--- 2_2_log_regression.py
+++ 2_2_log_regression.py
@@ -1,12 +1,7 @@
-import \
-    random
-
-import \
-    numpy as np
-import \
-    pandas as pd
-from sklearn.datasets import \
-    make_regression
+import random
+import pandas as pd
+import numpy as np
+from sklearn.datasets import make_regression
 
 X, y = make_regression(n_samples=1000, n_features=14, n_informative=10, noise=15, random_state=42)
 X = pd.DataFrame(X)
