055002f64b95f77f0485d19421299bc9c871a74f
nickolay <nickolay8@gmail.com>
2025-06-23 10:44:35 +0300

    progress on 3_1

---
+++ 3_1_knn.py
@@ -0,0 +1,112 @@
+import random
+import pandas as pd
+import numpy as np
+from sklearn.datasets import make_regression
+
+from sklearn.datasets import make_classification
+
+X, y = make_classification(n_samples=1000, n_features=14, n_informative=10, random_state=42)
+X = pd.DataFrame(X)
+y = pd.Series(y)
+X.columns = [f'col_{col}' for col in X.columns]
+
+class MyKNNClf:
+    def __init__(self, k=3, metric='euclidean', weight='uniform'):
+        self.k = k
+        self.train_size = 0
+        self.features = None
+        self.values = None
+        self.metric = metric
+        self.weight = weight
+
+    def __str__(self):
+        return f'MyKNNClf class: k={self.k}'
+
+    def fit(self, X, y):
+        self.features = X.to_numpy()
+        self.values = y.to_numpy()
+        self.train_size = self.features.shape
+
+    def get_distance(self, point1, point2):
+        if (self.metric == 'euclidean'):
+            return np.sqrt(np.sum((point1 - point2) ** 2, axis=1))
+        elif self.metric == 'chebyshev':
+            return np.abs(point1 - point2).max(axis=1)
+        elif self.metric == 'manhattan':
+            return np.abs(point1 - point2).sum(axis=1)
+        elif self.metric == 'cosine':
+            scalar_prod = (point1 * point2).sum(axis=1)
+            distance_prod = np.sqrt((point1 ** 2).sum(axis=1)) * np.sqrt((point2 ** 2).sum(axis=1))
+
+            return 1 - scalar_prod / distance_prod
+        else:
+            # should not happen
+            raise RuntimeError(f"Unknown metric={self.metric}")
+
+    def predict(self, X):
+        features = X.to_numpy()
+
+        res = []
+
+        for xi in features:
+            xi_repeated = np.repeat([ xi ], self.features.shape[-2], axis=0)
+            distances = self.get_distance(xi_repeated, self.features)
+
+            sorted_idx = np.argsort(distances, axis=0)
+            classes = self.values[ sorted_idx[0:self.k] ]
+            sorted_distances = distances[ sorted_idx[0:self.k] ]
+
+            if self.weight == 'uniform':
+                res.append(1 if classes.sum() >= self.k / 2 else 0)
+            elif self.weight == 'rank':
+                ranks = np.array(range(self.k + 1)[1:])
+                zero_ranks = np.where(classes == 0, 1 / ranks, 0)
+                one_ranks = np.where(classes == 1, 1 / ranks, 0)
+
+                res.append(0 if zero_ranks.sum() > one_ranks.sum() else 1)
+
+            elif self.weight == 'distance':
+                zero_ranks = np.where(classes == 0, 1 / sorted_distances, 0)
+                one_ranks = np.where(classes == 1, 1 / sorted_distances, 0)
+
+                res.append(0 if zero_ranks.sum() > one_ranks.sum() else 1)
+            else:
+                raise RuntimeError(f"Unknown weight={self.weight}")
+
+        return np.array(res)
+
+    def predict_proba(self, X):
+        features = X.to_numpy()
+
+        res = []
+
+        for xi in features:
+            xi_repeated = np.repeat([ xi ], self.features.shape[-2], axis=0)
+            distances = self.get_distance(xi_repeated, self.features)
+
+            sorted_idx = np.argsort(distances, axis=0)
+            classes = self.values[sorted_idx[0:self.k]]
+            sorted_distances = distances[ sorted_idx[0:self.k] ]
+
+            if self.weight == 'uniform':
+                res.append(classes.sum() / self.k)
+            elif self.weight == 'rank':
+                ranks = np.array(range(self.k + 1)[1:])
+                one_ranks = np.where(classes == 1, 1 / ranks, 0)
+
+                res.append(one_ranks.sum() / (1 / ranks).sum())
+            elif self.weight == 'distance':
+                one_ranks = np.where(classes == 1, 1 / sorted_distances, 0)
+
+                res.append(one_ranks.sum() / (1 / sorted_distances).sum())
+            else:
+                raise RuntimeError(f"Unknown weight={self.weight}")
+
+        return np.array(res)
+
+
+reg = MyKNNClf(k=3)
+
+reg.fit(X, y)
+
+reg.predict(X)
