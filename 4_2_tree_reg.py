3f6f19b2d0f55e308fc83be915068e238638f312
nickolay <nickolay8@gmail.com>
2025-06-25 12:53:26 +0300

    4_2 progress

---
+++ 4_2_tree_reg.py
@@ -0,0 +1,244 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_diabetes
+
+data = load_diabetes(as_frame=True)
+X, y = data['data'], data['target']
+
+#-------------------------------------------
+def get_mse(targets):
+    if targets.size == 0:
+        return 0
+
+    return np.var(targets)
+
+def get_mse_gain(features, targets, split):
+    initial_mse = get_mse(targets)
+
+    sorted_idx = np.argsort(features)
+    sorted = features[ sorted_idx ]
+
+    idx = np.searchsorted(sorted, split, side='right')
+
+    left_idx = sorted_idx[:idx]
+    left_mse = get_mse(targets[ left_idx ])
+
+    right_idx = sorted_idx[idx:]
+    right_mse = get_mse(targets[ right_idx])
+
+    return initial_mse - (left_mse * left_idx.size / targets.size + right_mse * right_idx.size / targets.size)
+
+def get_best_split(X: pd.DataFrame, y, splits, criterion):
+    targets = y.to_numpy()
+
+    col_name = None
+    split_value = 0
+    ig = 0
+
+    for col in X.columns:
+        features = X[ col ].to_numpy()
+
+        if splits[ col ].size > 0:
+            delimiters = splits[ col ]
+        else:
+            unique = np.unique(features)
+
+            delimiters = (unique[0:-1] + unique[1:]) / 2
+
+            if delimiters.size == 0:
+                delimiters = unique
+
+        gains = np.array(
+            list(map(
+                lambda delim: get_mse_gain(features, targets, delim),
+                delimiters
+            ))
+        )
+
+        delim_sorted_idx = np.argsort(gains)
+        max_gain = gains[ delim_sorted_idx[ -1 ] ]
+
+        if (col_name == None or ig < max_gain):
+            col_name = col
+            split_value = delimiters[ delim_sorted_idx[ -1 ] ]
+            ig = max_gain
+
+    return col_name, split_value, ig
+
+
+#-------------------------------------------
+def for_each_leaf(node, func, level=0):
+    if (node[ 'type' ] == 'leaf'):
+        func(node, level)
+    else:
+        for_each_leaf(node[ 'left' ], func, level + 1)
+        for_each_leaf(node[ 'right' ], func, level + 1)
+
+
+def for_each_node(node, func, level=0, side=None):
+    if (node[ 'type' ] == 'leaf'):
+        if func(node, level, side) == False:
+            return
+    else:
+        func(node, level, side)
+        for_each_node(node[ 'left' ], func, level + 1, 'left')
+        for_each_node(node[ 'right' ], func, level + 1, 'right')
+
+
+#-------------------------------------------
+class MyTreeReg:
+    def __init__(self, max_depth=5, min_samples_split=2, max_leafs=20, bins=None, criterion='entropy'):
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.max_leafs = max_leafs
+        self.leafs_cnt = 0
+        self.root = None
+        self.bins = bins
+        self.splits = None
+        self.criterion = criterion
+        self.fi = {}
+        self.total_n = 0
+
+    def __str__(self):
+        return f'MyTreeReg class: max_depth={ self.max_depth }, min_samples_split={ self.min_samples_split }, max_leafs={ self.max_leafs }'
+
+    def build_tree(self, X, y, leaf_count, depth):
+        count = X.to_numpy().shape[-2]
+
+        if (
+            depth > 0 and (
+                count == 1
+                or self.leafs_cnt + 1 >= self.max_leafs or depth >= self.max_depth or count < self.min_samples_split
+            )
+        ):
+            self.leafs_cnt += 1
+
+            return { 'type' : 'leaf', 'predict' : y.to_numpy().mean() }
+        else:
+            col_name, split_value, ig = get_best_split(X, y, self.splits, self.criterion)
+
+            features = X[ col_name ].to_numpy()
+            targets = y.to_numpy()
+
+            sorted_idx = np.argsort(features)
+            sorted = features[ sorted_idx ]
+
+            idx = np.searchsorted(sorted, split_value, side='right')
+
+            if idx == 0 or idx == features.shape[-1]:
+                self.leafs_cnt += 1
+
+                return { 'type' : 'leaf', 'predict' : y.to_numpy().mean() }
+
+            lefts_idx = sorted_idx[ :idx ]
+            rights_idx = sorted_idx[ idx: ]
+
+            # reserve one node for the right branch
+            self.leafs_cnt += 1
+
+            left = self.build_tree(X.iloc[ lefts_idx ], y.iloc[ lefts_idx ], leaf_count + 2, depth + 1)
+
+            # release the reserve
+            self.leafs_cnt -= 1
+
+            return {
+                'type' : 'node',
+                'col_name' : col_name,
+                'split_value' : split_value,
+                'left' : left,
+                'right' : self.build_tree(X.iloc[ rights_idx ], y.iloc[ rights_idx ], leaf_count + 2, depth + 1),
+                'importance' : features.size / self.total_n * (
+                    get_mse(targets)
+                    - lefts_idx.size / features.size * get_mse(y.iloc[ lefts_idx ])
+                    - rights_idx.size / features.size * get_mse(y.iloc[ rights_idx ])
+                )
+            }
+
+    def fit(self, X, y):
+        def get_unique_splits(X):
+            unique = np.unique(X.to_numpy())
+
+            return (unique[0:-1] + unique[1:]) / 2
+
+        def get_histogram_splits(X):
+            histogram_counts, histogram_boundaries = np.histogram(X.to_numpy(), bins=self.bins)
+
+            return histogram_boundaries[ 1:-1 ]
+
+        def get_splits(X):
+            unique_splits = get_unique_splits(X)
+
+            if self.bins == None or unique_splits.size <= self.bins - 1:
+                return np.array([])
+            else:
+                return get_histogram_splits(X)
+
+        self.total_n = X.shape[0]
+
+        for col in X.columns:
+            self.fi[ col ] = 0
+
+        self.splits = X.apply(get_splits, axis=0)
+
+        self.leafs_cnt = 0
+        self.root = self.build_tree(X, y, 0, 0)
+
+        def count(node, _, __):
+            if node[ 'type' ] == 'node':
+                self.fi[ node[ 'col_name'] ] += node[ 'importance' ]
+
+        for_each_node(self.root, count)
+
+    def print_tree(self):
+        def printer(node, level, side):
+            indent_str = ' ' * (level * 4)
+
+            if (node[ 'type' ] == 'node'):
+                print(f'{ indent_str }{ node[ 'col_name' ] } > { node[ 'split_value' ] }')
+            else:
+                print(f'{ indent_str }leaf_{ side } = { node[ 'predict' ] }')
+
+        for_each_node(self.root, printer)
+
+    def predict(self, X):
+        def predict_single(x):
+            prob = 0
+
+            def predict(node):
+                nonlocal prob
+
+                if node[ 'type' ] == 'leaf':
+                    prob = node[ 'predict' ]
+                else:
+                    value = x[ node[ 'col_name' ] ]
+
+                    if value > node[ 'split_value' ]:
+                        predict(node[ 'right' ])
+                    else:
+                        predict(node[ 'left' ])
+
+            predict(self.root)
+
+            return prob
+
+        return X.apply(predict_single, axis=1)
+
+clf = MyTreeReg(max_depth=10, min_samples_split=40, max_leafs=21)
+
+clf.fit(X, y)
+
+count=0
+sum=0
+
+def cc(node):
+    global count, sum
+    count = count + 1
+    sum += node[ 'predict' ]
+
+for_each_leaf(clf.root, lambda node, __: cc(node) )
+
+print(count, sum)
+
+clf.print_tree()
+
+clf.predict_proba(X)
